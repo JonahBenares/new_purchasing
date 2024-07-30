945ffea7e1d3be8872335f37d45424d0099061e9
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2024-07-30 09:25:28 +0800

    background

--- resources/js/components/login.vue
+++ resources/js/components/login.vue
@@ -41,6 +41,7 @@
 								
 								<div class="my-3">
 									<a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">LOGIN</a>
+									<a href="dashboard"> dashboard</a>
 								</div>
 								
 								<div class="text-center mt-4 font-weight-light text-sm">
