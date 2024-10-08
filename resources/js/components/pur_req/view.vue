cea4c75a4ecfd164fda41084e301268fd4d767e4
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2024-10-08 08:38:08 +0800

    cancelled

--- resources/js/components/pur_req/view.vue
+++ resources/js/components/pur_req/view.vue
@@ -175,156 +175,156 @@
 		<div class="row">
 			<div class="col-12 grid-margin stretch-card">
 				<div class="card">
-				<div class="card-body">
 					<div class="py-2 px-2 bg-red-500">
 						<span class="font-bold text-white">CANCELLED</span>
 					</div>
-					<hr class="border-dashed mt-0">
-					<div class="pt-1" id="printable">
-						<div class="print:block hidden print:flex print:justify-center h-full" >
-							<img src="../../../images/bg_cancelled.png" alt="" class="absolute h-[420px] align-center opacity-100">
-						</div>
-						<div class="row">
-							<div class="col-lg-6 col-sm-6 col-md-6">
-								<span class="text-sm text-gray-700 font-bold pr-1">Purchase Request: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.location}}</span>
+					<div class="card-body">
+						<!-- <hr class="border-dashed mt-0"> -->
+						<div class="pt-1" id="printable">
+							<div class="print:block hidden print:flex print:justify-center h-full" >
+								<img src="../../../images/bg_cancelled.png" alt="" class="absolute h-[420px] align-center opacity-100">
 							</div>
-							<div class="col-lg-6 col-sm-6 col-md-6">
-								<span class="text-sm text-gray-700 font-bold pr-1">Date Prepared: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.date_prepared}}</span>
+							<div class="row">
+								<div class="col-lg-6 col-sm-6 col-md-6">
+									<span class="text-sm text-gray-700 font-bold pr-1">Purchase Request: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.location}}</span>
+								</div>
+								<div class="col-lg-6 col-sm-6 col-md-6">
+									<span class="text-sm text-gray-700 font-bold pr-1">Date Prepared: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.date_prepared}}</span>
+								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6 col-sm-6 col-md-6">
-								<span class="text-sm text-gray-700 font-bold pr-1">PR Number: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.pr_no}}</span>
+							<div class="row">
+								<div class="col-lg-6 col-sm-6 col-md-6">
+									<span class="text-sm text-gray-700 font-bold pr-1">PR Number: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.pr_no}}</span>
+								</div>
+								<div class="col-lg-6 col-sm-6 col-md-6">
+									<span class="text-sm text-gray-700 font-bold pr-1">New PR Number: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.site_pr}}</span>
+								</div>
 							</div>
-							<div class="col-lg-6 col-sm-6 col-md-6">
-								<span class="text-sm text-gray-700 font-bold pr-1">New PR Number: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.site_pr}}</span>
+							<div class="row">
+								<div class="col-lg-6 col-sm-6 col-md-6">
+									<span class="text-sm text-gray-700 font-bold pr-1">Department: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.department_name}}</span>
+								</div>
+								<div class="col-lg-4 col-sm-4 col-md-4">
+									<span class="text-sm text-gray-700 font-bold pr-1">Process Code: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.process_code}}</span>
+								</div>
+								<div class="col-lg-2 col-sm-2 col-md-2">
+									<span class="text-sm text-gray-700 font-bold pr-1">Urgency: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.urgency}}</span>
+								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6 col-sm-6 col-md-6">
-								<span class="text-sm text-gray-700 font-bold pr-1">Department: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.department_name}}</span>
-							</div>
-							<div class="col-lg-4 col-sm-4 col-md-4">
-								<span class="text-sm text-gray-700 font-bold pr-1">Process Code: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.process_code}}</span>
-							</div>
-							<div class="col-lg-2 col-sm-2 col-md-2">
-								<span class="text-sm text-gray-700 font-bold pr-1">Urgency: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.urgency}}</span>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<span class="text-sm text-gray-700 font-bold pr-1">End-Use: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.enduse}}</span>
-							</div>
-							<div class="col-lg-12">
-								<span class="text-sm text-gray-700 font-bold pr-1">Purpose: </span>
-								<span class="text-sm text-gray-700">{{get_prhead.purpose}}</span>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<table class="w-full table-bordered bg-transparent !text-xs mt-3">
-									<tr class="bg-gray-100">
-										<td class="p-1 uppercase text-center" width="5%">#</td>
-										<td class="p-1 uppercase text-center" width="7%">Qty</td>
-										<td class="p-1 uppercase text-center" width="7%">UOM</td>
-										<td class="p-1 uppercase" width="20%">PN No.</td>
-										<td class="p-1 uppercase" width="">Item Description</td>
-										<td class="p-1 uppercase" width="10%">WH Stocks</td>
-										<td class="p-1 uppercase" width="15%">Date Needed</td>
-										<td class="p-1 uppercase" width="15%">Recom Date</td>
-										<td class="p-1 uppercase po_buttons" width="6%" align="center">
-											<span>
-												<Bars3Icon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 " ></Bars3Icon>
-											</span>
-										</td>
-									</tr>
-									<tr v-for="(pd,index) in get_prdetails">
-										<td :class="(pd.status=='Cancelled') ? 'bg-red-100 p-1 text-center print:bg-red-100' : 'p-1 text-center'">{{index + 1}}</td>
-										<td :class="(pd.status=='Cancelled') ? 'bg-red-100 p-1 text-center print:bg-red-100' : 'p-1 text-center'">{{ pd.quantity }}</td>
-										<td :class="(pd.status=='Cancelled') ? 'bg-red-100 p-1 text-center print:bg-red-100' : 'p-1 text-center'">{{ pd.uom }}</td>
-										<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.pn_no }}</td>
-										<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.item_description }}</td>
-										<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.wh_stocks }}</td>
-										<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.date_needed }}</td>
-										<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">
-											<input type="date" class="w-full"  v-model="pd.recom_date" @change="updateRecomdate(pd.id)"  v-if="pd.status!='Cancelled'">
-											<input type="date" class="w-full bg-red-100 print:bg-red-100" v-model="pd.recom_date" @change="updateRecomdate(pd.id)" readonly v-else>
-										</td>
-										<td :class="(pd.status=='Cancelled') ? 'bg-red-100 text-center po_buttons p-0' : 'text-center po_buttons p-0'">
-											<div class="space-x-1" v-if="pd.status=='Cancelled'"></div>
-											<div class="space-x-1" v-else-if="pd.status=='Referred'">
-												<button type="button" class="btn btn-xs btn-danger p-1" @click="cancelPrdetails('no',pd.id)">
-													<XMarkIcon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 "></XMarkIcon>
-												</button>
-											</div>
-											<div class="space-x-1" v-else-if="pd.status!='Referred'">
-												<button class="btn btn-xs btn-info p-1" @click="openModalReferred(pd.id)" title="Refer">
-													<ArrowUpOnSquareStackIcon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 "></ArrowUpOnSquareStackIcon>
-												</button>
-												<button type="button" class="btn btn-xs btn-danger p-1" @click="cancelPrdetails('no',pd.id)">
-													<XMarkIcon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 "></XMarkIcon>
-												</button>
-											</div>
-										</td>
-									</tr>
-								</table>
-							</div>
-						</div>
-						<hr class="border-dashed">
-						<div>
-							<div class="row mt-4 mb-4" v-if="get_prhead.petty_cash=='1'">
+							<div class="row">
 								<div class="col-lg-12">
-									<table class="w-full text-xs">
-										<tr>
-											<td class="text-center" width="20%">Prepared by</td>
-											<td width="2%"></td>
-											<td class="text-center" width="20%">Recommending Approval</td>
-											<td width="2%"></td>
-											<td class="text-center" width="20%">Approved by</td>
+									<span class="text-sm text-gray-700 font-bold pr-1">End-Use: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.enduse}}</span>
+								</div>
+								<div class="col-lg-12">
+									<span class="text-sm text-gray-700 font-bold pr-1">Purpose: </span>
+									<span class="text-sm text-gray-700">{{get_prhead.purpose}}</span>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-lg-12">
+									<table class="w-full table-bordered bg-transparent !text-xs mt-3">
+										<tr class="bg-gray-100">
+											<td class="p-1 uppercase text-center" width="5%">#</td>
+											<td class="p-1 uppercase text-center" width="7%">Qty</td>
+											<td class="p-1 uppercase text-center" width="7%">UOM</td>
+											<td class="p-1 uppercase" width="20%">PN No.</td>
+											<td class="p-1 uppercase" width="">Item Description</td>
+											<td class="p-1 uppercase" width="10%">WH Stocks</td>
+											<td class="p-1 uppercase" width="15%">Date Needed</td>
+											<td class="p-1 uppercase" width="15%">Recom Date</td>
+											<td class="p-1 uppercase po_buttons" width="6%" align="center">
+												<span>
+													<Bars3Icon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 " ></Bars3Icon>
+												</span>
+											</td>
 										</tr>
-										<tr>
-											<td class="text-center border-b"><br></td>
-											<td></td>
-											<td class="text-center border-b"></td>
-											<td></td>
-											<td class="text-center border-b"></td>
-										</tr>
-										<tr>
-											<td class="text-center p-0">{{ prepared_by }}</td>
-											<td></td>
-											<td class="text-center p-0">{{ recommended_by }}</td>
-											<td></td>
-											<td class="text-center p-0">{{ approved_by }}</td>
-										</tr>
-										<tr>
-											<td class="text-center"><br><br></td>
-											<td></td>
-											<td class="text-center"></td>
-											<td></td>
-											<td class="text-center"></td>
+										<tr v-for="(pd,index) in get_prdetails">
+											<td :class="(pd.status=='Cancelled') ? 'bg-red-100 p-1 text-center print:bg-red-100' : 'p-1 text-center'">{{index + 1}}</td>
+											<td :class="(pd.status=='Cancelled') ? 'bg-red-100 p-1 text-center print:bg-red-100' : 'p-1 text-center'">{{ pd.quantity }}</td>
+											<td :class="(pd.status=='Cancelled') ? 'bg-red-100 p-1 text-center print:bg-red-100' : 'p-1 text-center'">{{ pd.uom }}</td>
+											<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.pn_no }}</td>
+											<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.item_description }}</td>
+											<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.wh_stocks }}</td>
+											<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">{{ pd.date_needed }}</td>
+											<td :class="(pd.status=='Cancelled') ? 'p-1 bg-red-100 print:bg-red-100' : 'p-1'">
+												<input type="date" class="w-full"  v-model="pd.recom_date" @change="updateRecomdate(pd.id)"  v-if="pd.status!='Cancelled'">
+												<input type="date" class="w-full bg-red-100 print:bg-red-100" v-model="pd.recom_date" @change="updateRecomdate(pd.id)" readonly v-else>
+											</td>
+											<td :class="(pd.status=='Cancelled') ? 'bg-red-100 text-center po_buttons p-0' : 'text-center po_buttons p-0'">
+												<div class="space-x-1" v-if="pd.status=='Cancelled'"></div>
+												<div class="space-x-1" v-else-if="pd.status=='Referred'">
+													<button type="button" class="btn btn-xs btn-danger p-1" @click="cancelPrdetails('no',pd.id)">
+														<XMarkIcon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 "></XMarkIcon>
+													</button>
+												</div>
+												<div class="space-x-1" v-else-if="pd.status!='Referred'">
+													<button class="btn btn-xs btn-info p-1" @click="openModalReferred(pd.id)" title="Refer">
+														<ArrowUpOnSquareStackIcon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 "></ArrowUpOnSquareStackIcon>
+													</button>
+													<button type="button" class="btn btn-xs btn-danger p-1" @click="cancelPrdetails('no',pd.id)">
+														<XMarkIcon fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 "></XMarkIcon>
+													</button>
+												</div>
+											</td>
 										</tr>
 									</table>
 								</div>
 							</div>
-						</div>
-						<div class="row my-2 po_buttons" > 
-							<div class="col-lg-12 col-md-12">
-								<div class="flex justify-center space-x-2">
-									<button type="submit" class="btn btn-danger mr-2 w-36" @click="cancelAllpr('no')" v-if="get_prhead.status!='Cancelled' && get_prhead.status!='Closed'">Cancel</button>
-									<button type="submit" class="btn btn-primary mr-2 w-36" @click="printDiv()">Print</button>
+							<hr class="border-dashed">
+							<div>
+								<div class="row mt-4 mb-4" v-if="get_prhead.petty_cash=='1'">
+									<div class="col-lg-12">
+										<table class="w-full text-xs">
+											<tr>
+												<td class="text-center" width="20%">Prepared by</td>
+												<td width="2%"></td>
+												<td class="text-center" width="20%">Recommending Approval</td>
+												<td width="2%"></td>
+												<td class="text-center" width="20%">Approved by</td>
+											</tr>
+											<tr>
+												<td class="text-center border-b"><br></td>
+												<td></td>
+												<td class="text-center border-b"></td>
+												<td></td>
+												<td class="text-center border-b"></td>
+											</tr>
+											<tr>
+												<td class="text-center p-0">{{ prepared_by }}</td>
+												<td></td>
+												<td class="text-center p-0">{{ recommended_by }}</td>
+												<td></td>
+												<td class="text-center p-0">{{ approved_by }}</td>
+											</tr>
+											<tr>
+												<td class="text-center"><br><br></td>
+												<td></td>
+												<td class="text-center"></td>
+												<td></td>
+												<td class="text-center"></td>
+											</tr>
+										</table>
+									</div>
+								</div>
+							</div>
+							<div class="row my-2 po_buttons" > 
+								<div class="col-lg-12 col-md-12">
+									<div class="flex justify-center space-x-2">
+										<button type="submit" class="btn btn-danger mr-2 w-36" @click="cancelAllpr('no')" v-if="get_prhead.status!='Cancelled' && get_prhead.status!='Closed'">Cancel</button>
+										<button type="submit" class="btn btn-primary mr-2 w-36" @click="printDiv()">Print</button>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
 				</div>
 			</div>
 		</div>
