89645d9ae5a8ba121d5ec716e98f5fe7385db1b5
Ge3PauL <glennpaul.cenpri@gmail.com>
2024-10-14 11:48:43 +0800

    fixed terms after canvass compelete display only terms per vendor not all terms

--- resources/js/components/pur_quote/view.vue
+++ resources/js/components/pur_quote/view.vue
@@ -588,8 +588,8 @@
 														<td width="10%"></td>
 													</tr>
 												</tbody>
-												<tbody v-else>
-													<tr v-for="(vt, index) in rfq_vendor_terms">
+												<tbody v-for="(vt, index) in rfq_vendor_terms" v-else>
+													<tr v-if="vt.rfq_vendor_id == rvi.rfq_vendor_id">
 														<td width="10%"></td>
 														<td width="1%">{{ letters[index] }}.</td>
 														<td width="40%">{{ vt.terms }}</td>
